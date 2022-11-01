f60397d356040639908c429fcb4df214ac1c9e93
sleepingzzs <abismar.chintan@gmail.com>
2022-11-01 15:25:23 +0530

    working on users

---
+++ pages/u/index.jsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useState } from "react";
+export default function Users() {
+	return (
+		<div className='flex flex-row items-center h-[90vh] align-middle justify-center'>
+			<h1>users</h1>
+		</div>
+	);
+}
